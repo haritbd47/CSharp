52c2a3840b2edc8e1fb3346f1221f13e3df368b9
USER <USER@MSI>
2023-05-02 17:12:08 +0700

    Add project files.

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Human
+{
+    public string name;
+    public int age;
+    public string gender;
+    public Human(string name, int age, string gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    public void Eat()
+    {
+        Console.WriteLine("eating");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("sleeping");
+    }
+}
+
+class Child : Human
+{
+    public string FavToy;
+    public Child(string name, int age, string gender, string favToy)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.FavToy = favToy;
+    }
+
+    public void Set(string toys)
+    {
+        FavToy = toys;
+        Console.WriteLine();
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using System;
+Console.Write("Input toys: ");
+string toys = Convert.ToString(Console.ReadLine);
+human test = new human("JJ", 22, "Male");
+
+//อธิบาย คือผมพยายามทำแค่ ชื่อ อายุ เพศ แล้วใน child ก็มีให้เซตของเล่นที่ชอบ(แต่มันไม่ได้ครับ)
